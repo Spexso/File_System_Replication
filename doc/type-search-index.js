3636f03df13354e10bf6dba3e012925c1c743cfe
Kağan <89082422+Spexso@users.noreply.github.com>
2024-05-11 02:36:48 +0300

    Javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Directory"},{"p":"<Unnamed>","l":"Explorer"},{"p":"<Unnamed>","l":"File"},{"p":"<Unnamed>","l":"FileSystem"},{"p":"<Unnamed>","l":"FileSystemElement"}];updateSearchResults();
